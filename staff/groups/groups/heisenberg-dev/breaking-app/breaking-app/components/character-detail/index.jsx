d1637c7721ff172adb3702f0c611aeccb0239ebf
Ruben Vidales <rubenvj85@gmail.com>
2019-10-31 16:25:35 +0100

    easter egg

--- staff/groups/groups/heisenberg-dev/breaking-app/breaking-app/components/character-detail/index.jsx
+++ staff/groups/groups/heisenberg-dev/breaking-app/breaking-app/components/character-detail/index.jsx
@@ -3,37 +3,40 @@ function CharacterDetail({ item: { char_id, name, birthday, occupation, img, sta
       event.preventDefault()
       onBackCharacters()
    }}>Go back to Characters</button>
-   <button className="register__goback" href="" onClick={event => {
-      event.preventDefault()
-      onBack()
-   }}>Go back to Search</button>
-       <article className="character-detail">
-      <h3 className="character-detail__title">{name}</h3>
-      <h4 className="character-detail__nickname">{nickname}</h4>
-      <img className="character-detail__image" src={img} alt="character detail image" />
-      <h4 className="character-detail__birthday-title">Birthday:</h4>
-      <span className="character-detail__birthday-text">{birthday}</span>
-      <h4 className="character-detail__status-title">Status:</h4>
-      <span className="character-detail__status-text">{status}</span>
-      <h4 className="character-detail__ocupation-title">Ocupation:</h4>
-      <ul className="character-detail__ocupation-list">
-         {occupation.map((element) => <li key={element} className="character-detail__occupation-item" >{element}</li>)}
-      </ul>
-      <h4 className="character-detail__description-title">Description:</h4>
-      <p className="character-detail__description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Optio
-          blanditiis natus illo enim deserunt cupiditate et odio est porro eaque, impedit accusamus
-                    saepe eum a adipisci facilis harum totam velit!</p>
-      <h4 className="character-detail__portayed-title">Portayed by:</h4>
-      <span className="character-detail__portrayed-text">{portrayed}</span>
-      <div className="break"></div>
-      <span className="character-detail__fav" onClick={event => {
+      <button className="register__goback" href="" onClick={event => {
          event.preventDefault()
-         event.stopPropagation()
+         onBack()
+      }}>Go back to Search</button>
+      <article className="character-detail">
+         <h3 className="character-detail__title">{name}</h3>
+         <h4 className="character-detail__nickname">{nickname}</h4>
+         <img className="character-detail__image" src={img} alt="character detail image" />
+         <h4 className="character-detail__birthday-title">Birthday:</h4>
+         <span className="character-detail__birthday-text">{birthday}</span>
+         <h4 className="character-detail__status-title">Status:</h4>
+         <span className="character-detail__status-text">{status}</span>
+         <h4 className="character-detail__ocupation-title">Ocupation:</h4>
+         <ul className="character-detail__ocupation-list">
+            {occupation.map((element) => <li key={element} className="character-detail__occupation-item" >{element}</li>)}
+         </ul>
+         <h4 className="character-detail__description-title">Description:</h4>
+         <p className="character-detail__description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Optio
+             blanditiis natus illo enim deserunt cupiditate et odio est porro eaque, impedit accusamus
+                    saepe eum a adipisci facilis harum totam velit!</p>
+         <h4 className="character-detail__portayed-title">Portayed by:</h4>
+         <span className="character-detail__portrayed-text">{portrayed}</span>
+         <div className="break"></div>
+         <span className="character-detail__fav" onClick={event => {
+            event.preventDefault()
+            event.stopPropagation()
 
-         onFav(char_id, true)
-      }}>{isFav ? '🧡' : '💔'}</span>
+            //Skyker easter egg
+            if (char_id == '3' && isFav === false) alert('WTF? Nobody loves Skyler')
 
-   </article>
+            onFav(char_id, true)
+         }}>{isFav ? '🧡' : '💔'}</span>
+
+      </article>
    </>
 }
--- staff/groups/groups/heisenberg-dev/breaking-app/breaking-app/components/character-list/index.jsx
+++ staff/groups/groups/heisenberg-dev/breaking-app/breaking-app/components/character-list/index.jsx
@@ -1,19 +1,23 @@
 function CharacterItem({ item: { char_id: charId, name, img, isFav }, onClickCharacter, onFav }) {
-   return <li className="characters__list-item">
-       <a href="" className="characters__list-link" onClick={event => {
-           event.preventDefault()
-           onClickCharacter(charId.toString())
-       }}>
-           <article className="character">
-           <h2 className="character__title">{name}</h2>
-           <img className="character__image" src={img} />
-           <p className="character_description">Lorem ipsum</p>
-           <span className="character__fav" onClick={event => {
-               event.preventDefault()
-               event.stopPropagation()
-               onFav(charId.toString())
-           }}>{isFav ? '🧡' : '💔'}</span>
-           </article>
-       </a>
-   </li>
+    return <li className="characters__list-item">
+        <a href="" className="characters__list-link" onClick={event => {
+            event.preventDefault()
+            onClickCharacter(charId.toString())
+        }}>
+            <article className="character">
+                <h2 className="character__title">{name}</h2>
+                <img className="character__image" src={img} />
+                <p className="character_description">Lorem ipsum</p>
+                <span className="character__fav" onClick={event => {
+                    event.preventDefault()
+                    event.stopPropagation()
+
+                    //Skyker easter egg
+                    if (charId == '3' && isFav === false) alert('WTF? Nobody loves Skyler')
+
+                    onFav(charId.toString())
+                }}>{isFav ? '🧡' : '💔'}</span>
+            </article>
+        </a>
+    </li>
 }

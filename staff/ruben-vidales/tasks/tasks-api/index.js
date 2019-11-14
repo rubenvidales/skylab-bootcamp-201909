8351d68234277001aa5ef1f92490d22cf4751139
Ruben Vidales <rubenvj85@gmail.com>
2019-11-14 12:59:45 +0100

    corrections

--- staff/ruben-vidales/tasks/tasks-api/index.js
+++ staff/ruben-vidales/tasks/tasks-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { name, version } = require('./package.json')
+const jwt = require('jsonwebtoken')
 
 const api = express()
 

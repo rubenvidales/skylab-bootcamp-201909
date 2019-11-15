88a9bd8ae798449cd9faf4cdb72964027859ede0
Ruben Vidales <rubenvj85@gmail.com>
2019-11-15 09:02:02 +0100

    add create task logic

--- staff/ruben-vidales/tasks/tasks-api/logic/create-task/index.js
+++ staff/ruben-vidales/tasks/tasks-api/logic/create-task/index.js
@@ -1,25 +1,32 @@
 const validate = require('../../utils/validate')
-const tasks = require('../../data/tasks')('test')
+const users = require('../../data/users')()
+const tasks = require('../../data/tasks')()
 const uuid = require('uuid/v4')
-const { ConflictError } = require('../../utils/errors')
+const { NotFoundError } = require('../../utils/errors')
 
-module.exports = function (user, title, description, status) {
-    validate.string(user)
-    validate.string.notVoid('user', name)
+module.exports = function (userId, title, description) {
+    validate.string(userId)
+    validate.string.notVoid('userId', userId)
     validate.string(title)
     validate.string.notVoid('title', title)
     validate.string(description)
     validate.string.notVoid('description', description)
-    validate.string(status)
-    validate.string.notVoid('status', status)
-
 
     return new Promise((resolve, reject) => {
-        const id = uuid()
-        const insertedTask = {id, user, title, description, status, date: new Date()}
+        const user = users.data.find(user => user === userId)
+        if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+
+        const task = {
+            id: uuid(),
+            user, 
+            title, 
+            description, 
+            status: 'TODO', 
+            date: new Date
+        }
 
         tasks.data.push(task)
 
-        tasks.persist().then(resolve(task.id)).catch(reject)
+        tasks.persist().then(()=> resolve(task.id)).catch(reject)
     })
 }

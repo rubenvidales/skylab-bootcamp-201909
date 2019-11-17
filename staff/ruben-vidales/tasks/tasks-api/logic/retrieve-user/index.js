5ecc61d4b9a233d02314d6cb3695e6b2f14142f8
Ruben Vidales J <rubenvj85@gmail.com>
2019-11-17 17:26:31 +0100

    corrections in users logic and testing

--- staff/ruben-vidales/tasks/tasks-api/logic/retrieve-user/index.js
+++ staff/ruben-vidales/tasks/tasks-api/logic/retrieve-user/index.js
@@ -1,22 +1,33 @@
 const validate = require('../../utils/validate')
-const users = require('../../data/users')()
 const { NotFoundError } = require('../../utils/errors')
+const database = require('../../utils/database')
+const { ObjectId } = database
 
 module.exports = function (id) {
     validate.string(id)
     validate.string.notVoid('id', id)
 
-    return new Promise((resolve, reject) => {
-        const user = users.data.find(user => user.id === id)
+    const client = database()
 
-        if (!user) return reject(new NotFoundError(`user with id ${id} not found`))
+    return client.connect()
+        .then (connection => {
+            const users = connection.db().collection('users')
 
-        user.lastAccess = new Date
+            return users.findOne({ _id: ObjectId(id) })
+                .then( user => {
+                    if(!user) throw new NotFoundError(`user with id ${id} not found`)
+                    
+                    return users.updateOne({username: user.username},{$set:{lastAccess: new Date}})
+                        .then(result => {
+                            if (!result.modifiedCount) throw Error('could not update user')
 
-        users.persist().then(() => {
-            const { name, surname, email, username } = user
-
-            resolve({ id, name, surname, email, username })
+                            delete user.password
+                            return user
+                        })                   
+                })                
         })
-    })
+        //Syncronous error if ObjectId can not create the id
+        .catch( error => {
+            if (error) throw new NotFoundError(`user with id ${id} not found`)
+        })
 }
--- staff/ruben-vidales/tasks/tasks-api/logic/retrieve-user/index.spec.js
+++ staff/ruben-vidales/tasks/tasks-api/logic/retrieve-user/index.spec.js
@@ -1,38 +1,46 @@
+require('dotenv').config()
+const { env: { DB_URL_TEST } } = process
 const { expect } = require('chai')
 const { random } = Math
-const users = require('../../data/users')('test')
 const retrieveUser = require('.')
 const uuid = require('uuid/v4')
 const { NotFoundError } = require('../../utils/errors')
+const database = require('../../utils/database')
+const { ObjectId } = database
 
-describe.skip('logic - retrieve user', () => {
-    before(() => users.load())
+describe('logic - retrieve user', () => {
+    let client, users
+    before(() => {
+        client = database(DB_URL_TEST)
+        return client.connect()
+            .then(connection => users = connection.db().collection('users'))
+    })
 
     let id, name, surname, email, username, password
 
     beforeEach(() => {
-        id = uuid()
         name = `name-${random()}`
         surname = `surname-${random()}`
         email = `email-${random()}@mail.com`
         username = `username-${random()}`
         password = `password-${random()}`
 
-        users.data.push({ id, name, surname, email, username, password })
+        return users.insertOne({ name, surname, email, username, password })
+            .then(({ insertedId }) => id = insertedId.toString())
     })
 
-    it('should succeed on correct user id', () =>
-        retrieveUser(id)
+    it('should succeed on correct user id', () => {
+        return retrieveUser(id)
             .then(user => {
                 expect(user).to.exist
-                expect(user.id).to.equal(id)
+                expect(ObjectId(user._id).toString()).to.equal(id)
                 expect(user.name).to.equal(name)
                 expect(user.surname).to.equal(surname)
                 expect(user.email).to.equal(email)
                 expect(user.username).to.equal(username)
                 expect(user.password).to.be.undefined
             })
-    )
+    })
 
     it('should fail on wrong user id', () => {
         const id = 'wrong'
@@ -49,4 +57,5 @@ describe.skip('logic - retrieve user', () => {
     })
 
     // TODO other cases
+    after(() => client.close())
 })

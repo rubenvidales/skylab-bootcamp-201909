c18180f117e44aacd184545c9cf5677fda80abc3
Ruben Vidales J <rubenvj85@gmail.com>
2019-11-19 00:28:04 +0100

    end api logic and testing

--- staff/ruben-vidales/tasks/tasks-api/logic/index.js
+++ staff/ruben-vidales/tasks/tasks-api/logic/index.js
@@ -3,5 +3,7 @@ module.exports = {
     registerUser: require('./register-user'),
     retrieveUser: require('./retrieve-user'),
     createTask: require('./create-task'),
-    listTasks: require('./list-tasks')
+    listTasks: require('./list-tasks'),
+    modifyTask: require('./modify-task'),
+    removeTask: require('./remove-task')
 }

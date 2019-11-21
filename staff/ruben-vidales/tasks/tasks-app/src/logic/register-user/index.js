0fb99e593025d4f1e417786b41cad89b544bd4c0
Ruben Vidales <rubenvj85@gmail.com>
2019-11-21 23:06:02 +0100

    start react logic and testing

--- staff/ruben-vidales/tasks/tasks-app/src/logic/register-user/index.js
+++ staff/ruben-vidales/tasks/tasks-app/src/logic/register-user/index.js
@@ -1,11 +1,29 @@
-export default function (name, surname, email, username, password) {
-	// TODO validate all fields
+const call = require('../../utils/call')
+const { validate } = require('tasks-util')
+const { env: { REACT_APP_API_URL: API_URL } } = process
 
-	return fetch('http://192.168.0.41:8000/users', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ name, surname, email, username, password })
-	})
-		.then(res => res.status === 201 ? undefined : res.json().then(({ message }) => { throw Error(message) }))
+module.exports =  function (name, surname, email, username, password) {
+    validate.string(name)
+    validate.string.notVoid('name', name)
+    validate.string(surname)
+    validate.string.notVoid('surname', surname)
+    validate.string(email)
+    validate.string.notVoid('e-mail', email)
+    validate.email(email)
+    validate.string(username)
+    validate.string.notVoid('username', username)
+    validate.string(password)
+	validate.string.notVoid('password', password)
+	
+	return (async () => {
+		const res = await call(`${API_URL}/users`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name, surname, email, username, password })
+		})
 
+		if(res.status === 201) return
+
+		throw new Error(JSON.parse(res.body).message)
+	})()
 }

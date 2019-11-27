fc8f91248a2320a3bc7b467604f0feada6dcfa58
Ruben Vidales <rubenvj85@gmail.com>
2019-11-27 12:47:58 +0100

    correction in converter

--- staff/ruben-vidales/quickshare/quickshare-util/index.js
+++ staff/ruben-vidales/quickshare/quickshare-util/index.js
@@ -2,5 +2,6 @@ module.exports = {
     errors: require('./errors'),
     validators: require('./validators'),
     polyfills: require('./polyfills'),
-    validate: require('./validate')
+    validate: require('./validate'),
+    converter: require('./converter')
 }

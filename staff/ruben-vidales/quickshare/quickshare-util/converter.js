b66ae4931df303d712af092f030d2073d8d89426
Ruben Vidales <rubenvj85@gmail.com>
2019-11-27 15:49:33 +0100

    improvents in converter

--- staff/ruben-vidales/quickshare/quickshare-util/converter.js
+++ staff/ruben-vidales/quickshare/quickshare-util/converter.js
@@ -1,10 +1,29 @@
 const converter = {
     stringToSeconds(target) {
         const pieces = target.split(':')
-        return (+pieces[0]) * 60 * 60 + (+pieces[1]) * 60 + (+pieces[2])
+        //Normal path: hh:mm:ss (máx 23:59:59)
+        if (pieces.length == 3){
+            return (+pieces[0]) * 60 * 60 + (+pieces[1]) * 60 + (+pieces[2])
+        //Special path mm:ss 
+        }else if (pieces.length === 2){
+            return (+pieces[0]) * 60 + (+pieces[1])
+        }else{
+            return (+pieces[0])
+        }
+        
     },
     secondsToString(seconds) {
-        return new Date(seconds * 1000).toISOString().substr(11,8)
+        //return new Date(seconds * 1000).toISOString().substr(11,8)
+        let hrs = Math.floor(seconds/3600)
+        seconds  %= 3600
+        let mins = Math.floor(seconds/60)
+        let secs = seconds%60
+
+        hrs = String(hrs).padStart(2,'0')
+        mins = String(mins).padStart(2,'0')
+        secs = String(secs).padStart(2,'0')
+
+        return (hrs+':'+mins+':'+secs)
     }
 }
 

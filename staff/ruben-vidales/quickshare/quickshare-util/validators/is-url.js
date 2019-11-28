fd917726f72ed9b73089037d7e27d4a5c9466cf4
Ruben Vidales <rubenvj85@gmail.com>
2019-11-28 10:17:58 +0100

    add new validations

---
+++ staff/ruben-vidales/quickshare/quickshare-util/validators/is-url.js
@@ -0,0 +1,5 @@
+const URL_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+
+module.exports = function (target) {
+    return URL_REGEX.test(target)
+}
